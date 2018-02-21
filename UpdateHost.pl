c8947fb5baba0a736dc2968de008f463e8f8225b
jtwiborg <36697170+jtwiborg@users.noreply.github.com>
2018-02-21 20:00:50 +0100

    Created UpdateHost.pl

---
+++ UpdateHost.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+use md5ssh;
+
+die "Usage: $0 <host>\n" unless @ARGV == 1;
+my $host = shift;
+	
+my @bata = md5ssh::CheckHost($host);
+foreach my $ret(@bata) {
+	my ($hst, $f, $h, $n) = split(/:/, $ret);
+	print "Checksum Update: $f:$h -> $n\n";
+	md5ssh::UpdateFile("$host:$f");
+}
+exit;
